b100d9eb65a2ff8950fb51124335c204684abe47
Rafael Oliveira <rafigor@gmail.com>
2021-04-02 15:29:55 -0300

    Method ShowMeTheCode

---
+++ CalculaJurosAPI/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using CalculaJurosAPI.Models;
+
+namespace CalculaJurosAPI.Controllers
+{
+    [ApiController]
+    [Route("showmethecode")]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        private readonly ShowMeTheCode _showMeTheCode;
+
+        public ShowMeTheCodeController()
+        {
+            _showMeTheCode = new ShowMeTheCode();
+        }
+
+        [HttpGet()]
+        public ActionResult<ShowMeTheCode> Get()
+        {
+            return _showMeTheCode;
+        }
+    }
+}
---
+++ CalculaJurosAPI/Models/ShowMeTheCode.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculaJurosAPI.Models
+{
+    public class ShowMeTheCode
+    {
+        public string url
+        {
+            get { return "https://github.com/rafigor/testeApi"; }
+        }
+    }
+}
