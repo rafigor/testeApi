257feb23c37b33b5852d2dc8f5e312b5b41d985a
Rafael Oliveira <rafigor@gmail.com>
2021-04-02 15:23:25 -0300

    Calculo de Juros usando API

--- CalculaJurosAPI/Controllers/CalculaJurosController.cs
+++ CalculaJurosAPI/Controllers/CalculaJurosController.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using CalculaJurosAPI.Models;
+using Newtonsoft.Json;
 
 namespace CalculaJurosAPI.Controllers
 {
@@ -16,9 +15,20 @@ namespace CalculaJurosAPI.Controllers
         [HttpGet()]
         public ActionResult<CalculaJuros> Get([FromQuery(Name = "valorinicial")] decimal valorinicial, [FromQuery(Name = "meses")] int meses)
         {
-            var _taxaJuros = 0.01d;
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("http://localhost:8080/taxaJuros").Result;
+            response.EnsureSuccessStatusCode();
+            string conteudo = response.Content.ReadAsStringAsync().Result;
+
+            dynamic result = JsonConvert.DeserializeObject(conteudo);
+
+            decimal _taxaJuros = result.taxaJuros + 1d;
             _calculaJuros = new CalculaJuros();
-            _calculaJuros.valorfinal = (decimal.Truncate(valorinicial * Convert.ToDecimal(Math.Pow((1d + _taxaJuros), meses))*100)/100)
+            _calculaJuros.valorfinal = (decimal.Truncate(valorinicial * Convert.ToDecimal(Math.Pow(Convert.ToDouble(_taxaJuros), meses))*100)/100)
                 .ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
             return _calculaJuros;
         }    
